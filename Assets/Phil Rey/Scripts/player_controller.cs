d56cf63d7adbcb665dc53e5f34e579b7a368d8ed
Phil Rey <32130982+Philrey@users.noreply.github.com>
2021-08-16 00:40:22 +0800

    Added Characters

---
+++ Assets/Phil Rey/Scripts/player_controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    private void Awake() {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
